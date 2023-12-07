d74c373d4673c330a310e8cb26f247f0840aaa66
Alisa <alisa.melissa@icloud.com>
2023-12-07 15:08:12 +0200

    Initial Commit

---
+++ ParallaxReel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParallaxReel
+//
+//  Created by Алиса Третьякова on 07.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParallaxReel/ParallaxReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxReelApp.swift
+//  ParallaxReel
+//
+//  Created by Алиса Третьякова on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxReelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
