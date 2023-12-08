ff2a6c6b18d28bbfe3b0e8832d351b30c09449df
Alisa <alisa.melissa@icloud.com>
2023-12-08 16:22:05 +0200

    Parallax

---
+++ ParallaxReel/Color+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extension.swift
+//  ParallaxReel
+//
+//  Created by Алиса Третьякова on 07.12.2023.
+//
+
+import Foundation
---
+++ ParallaxReel/Model/GuitarCard.swift
@@ -0,0 +1,8 @@
+//
+//  GuitarCard.swift
+//  ParallaxReel
+//
+//  Created by Алиса Третьякова on 07.12.2023.
+//
+
+import Foundation
